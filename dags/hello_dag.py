21707c538dd7676f6f1b4e21bfcd8c4f1d8b33bd
Jayachandra Kasarla <jayachandra.kasarla@astronomer.io>
2026-08-14 15:35:37 +0530

    initial push

---
+++ dags/hello_dag.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from airflow.sdk import Variable, dag, task
+from pendulum import datetime
+
+
+@dag(
+    schedule=None,
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=["example", "secrets"],
+)
+def hello_dag():
+    @task
+    def print_hello_world():
+        value = Variable.get("hello-world")
+        print(f"hello-world = {value}")
+
+    print_hello_world()
+
+
+hello_dag()
