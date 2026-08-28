3a38379d58f266d1ac914e6525e355bf46767fbb
Jayachandra Kasarla <jayachandra.kasarla@astronomer.io>
2026-08-28 14:05:44 +0530

    add back hello dags

---
+++ dags/hello_dag.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from airflow.sdk import Variable, dag, task
+from pendulum import datetime
+from k8s import pod
+
+@dag(
+    schedule=None,
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=["example", "secrets"],
+)
+def hello_dag():
+    @task
+    def print_hello_world():
+        value = Variable.get("hello-world")
+        print(f"hello-world = {value}")
+
+        return value
+
+    @task
+    def print_value(val):
+        print(f"value from prev task: {val}")
+
+    t1 = print_hello_world()
+    t2 = print_value(t1)
+
+    t1 >> t2
+
+hello_dag()
