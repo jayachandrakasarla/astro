be8460888c028faba363506b025e988f34eb8e15
Jayachandra Kasarla <jayachandra.kasarla@astronomer.io>
2026-08-31 15:44:18 +0530

    added to slow dag to test dag parsing metrics on prometheus

---
+++ dags/slow_dag.py
@@ -0,0 +1,21 @@
+import datetime
+from airflow.sdk import dag, task
+import time
+
+#sleep for 10 seconds at top level
+time.sleep(10)
+
+@dag(
+    dag_id="slow_dag",
+    start_date=datetime.datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+)
+def slow_dag():
+    @task
+    def hello_airflow():
+        print("Hello from Airflow 3!")
+
+    hello_airflow()
+
+slow_dag()
