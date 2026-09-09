cd72de4a0b2c5f94baec200446639b83aed799aa
Jayachandra Kasarla <jayachandra.kasarla@astronomer.io>
2026-09-09 17:46:38 +0530

    testing a custom folder import in include

--- include/probe_lib.py
+++ include/probe_lib.py
@@ -1,5 +1,8 @@
+from some_folder.some_file import some_function
+
 PROBE_MESSAGE = "include.probe_lib imported successfully"
 
 
 def get_probe_message() -> str:
+    some_function()
     return PROBE_MESSAGE
